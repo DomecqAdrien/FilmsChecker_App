1207406613ff0847f6914a9caf00cbcb9ba34cb2
Adrien DOMECQ <kaguraproject.sa@gmail.com>
2021-12-05 23:27:25 +0100

    app

---
+++ app/src/main/java/com/example/filmschecker/FilmDetailActivity.kt
@@ -0,0 +1,192 @@
+package com.example.filmschecker
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.filmschecker.adapter.ActorsAdapter
+import com.example.filmschecker.databinding.ActivityFilmsBinding
+import com.example.filmschecker.domain.*
+import com.example.filmschecker.service.FilmService
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.stream.Collectors
+
+
+class FilmDetailActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+    private lateinit var binding: ActivityFilmsBinding
+    private lateinit var retrofit: Retrofit
+    private lateinit var filmService: FilmService
+    private lateinit var film: Film
+    private var filmId: Int = 0
+    private lateinit var actorsRecyclerView: RecyclerView
+    private lateinit var actors : List<Actor>
+    private lateinit var crew : List<Crew>
+    private lateinit var actorsAdapter : ActorsAdapter
+    private lateinit var userEmail :String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFilmsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        actorsRecyclerView = binding.actorsRecyclerView
+        initRecyclerView()
+        filmId = this.intent.extras!!.getInt("film_id")
+        userEmail = FirebaseAuth.getInstance().currentUser?.email.toString()
+
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(getString(R.string.tmdb_base_url))
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        filmService = retrofit.create(FilmService::class.java)
+
+
+        getFilm()
+    }
+
+    private fun initRecyclerView() {
+        actorsAdapter = ActorsAdapter()
+        actorsRecyclerView.apply {
+            adapter = actorsAdapter
+        }
+    }
+
+    private fun getFilm() {
+        val filmsCall : Call<Film> = filmService.getOneFilm(filmId)
+        filmsCall.enqueue(object: Callback<Film> {
+            override fun onResponse(call: Call<Film>, response: Response<Film>) {
+                film = response.body()!!
+                initFilm()
+                getActors()
+            }
+            override fun onFailure(call: Call<Film>, t: Throwable) {
+                Log.i("error",t.message.toString())
+            }
+        })
+    }
+
+    private fun getActors() {
+
+        val creditsCall : Call<APIParserDTO> = filmService.getActorsByFilm(filmId)
+
+        creditsCall.enqueue(object : Callback<APIParserDTO>{
+            override fun onResponse(call: Call<APIParserDTO>, response: Response<APIParserDTO>) {
+                actors = response.body()?.actors?: emptyList()
+                crew = response.body()?.crew?: emptyList()
+                Log.i("crew size",crew.size.toString())
+                actorsAdapter.actors = actors
+            }
+
+            override fun onFailure(call: Call<APIParserDTO>, t: Throwable) {}
+        })
+    }
+
+
+
+    private fun initFilm() {
+        with(binding){
+
+            btnCommentaires.setOnClickListener {
+                val intent = Intent(this@FilmDetailActivity, CommentairesActivity::class.java)
+                startActivity(Intent())
+            }
+
+            manageFavori(true)
+            Glide.with(applicationContext)
+                .load("https://image.tmdb.org/t/p/w500"+film.affiche)
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(filmAffiche)
+            filmTitle.text = film.title
+            filmDate.text = film.date!!.subSequence(0,4)
+            filmRestriction.text = film.restriction
+            val heure = film.runtime / 60
+            val minute = film.runtime % 60
+            filmDuration.text = "$heure h $minute min"
+            filmDescription.text = film.overview
+            var genres = ""
+            for(i in film.genres.indices){
+                genres += "${film.genres[i].name}"
+                if (i != film.genres.size.minus(1)) {
+                    genres += ", "
+                }
+            }
+            filmCategories.text = genres
+            filmNote.text = "${film.voteAverage}/10"
+
+            val drawable = when {
+                film.popularity > 65 -> getDrawable(R.drawable.square_green)
+                film.popularity > 40 -> getDrawable(R.drawable.square_orange)
+                else -> getDrawable(R.drawable.square_red)
+            }
+            filmScoreSquare.setImageDrawable(drawable)
+            filmPopularity.text = film.popularity.toInt().toString()
+
+        }
+    }
+
+    private fun manageFavori(isCheckButton: Boolean){
+        database = FirebaseDatabase.getInstance().reference
+        database.addListenerForSingleValueEvent(object : ValueEventListener {
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.i("error",error.message)
+            }
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.hasChild("favoris/$userEmail-$filmId".replace(".","*"))){
+                    if(!isCheckButton){
+                        removeFavori()
+                    }else{
+                        binding.filmManageFavori.setBackgroundColor(getColor(R.color.herbGreen))
+                        binding.filmManageFavori.text = getText(R.string.favori)
+                    }
+                    Log.i("data","EXISTS")
+                }
+
+                else{
+                    Log.i("data","EXISTS PAS")
+                    if(!isCheckButton)
+                        addFavori()
+                }
+            }
+        })
+    }
+    fun onClickFavori(v: View?=null){
+        manageFavori(false)
+    }
+
+    private fun addFavori() {
+        val f = Favori(filmId, userEmail)
+        Log.i("dburl", database.toString())
+        database.child("favoris").child("$userEmail-$filmId".replace(".", "*")).setValue(f)
+            .addOnSuccessListener {
+                Log.i("db", "success")
+                with(binding) {
+                    filmManageFavori.setBackgroundColor(getColor(R.color.herbGreen))
+                    filmManageFavori.text = getText(R.string.favori)
+                }
+            }
+            .addOnFailureListener {
+                Log.i("db", it.toString())
+            }
+    }
+
+    private fun removeFavori(){
+        Log.i("data","REMOVE FAV")
+        database.child("favoris").child("$userEmail-$filmId".replace(".", "*")).removeValue()
+        with(binding){
+            filmManageFavori.setBackgroundColor(getColor(R.color.holoDarkBlue))
+            filmManageFavori.text = getText(R.string.addFav)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/filmschecker/service/FilmService.kt
@@ -0,0 +1,29 @@
+package com.example.filmschecker.service
+
+import com.example.filmschecker.domain.*
+import retrofit2.Call
+import retrofit2.http.*
+
+
+interface FilmService {
+    @GET("genres")
+    fun getGenres(): Call<List<Category>>
+
+    @GET("films/{page}")
+    fun getAllFilms(@Path("page") nbPage: Int): Call<List<Film>>
+
+    @GET("film/{film_id}")
+    fun getOneFilm(@Path("film_id") filmId: Int) : Call<Film>
+
+    @GET("film/{film_id}/actors")
+    fun getActorsByFilm(@Path("film_id") filmId: Int): Call<APIParserDTO>
+
+    @GET("search")
+    fun getFilmsBySearch(@Query("query") query: String): Call<List<Film>>
+
+    @GET("film/{movieId}/user/{userEmail}/note/{note}")
+    fun rateMovie(@Path("movieId") movieId: Int, @Path("userEmail") email: String, @Path("note") note: Int)
+
+    @POST("film/{movieId}/user/{userEmail}/comment")
+    fun commentMovie(@Path("movieId") movieId: Int, @Path("userEmail") email: String, @Body comment: Int)
+}
